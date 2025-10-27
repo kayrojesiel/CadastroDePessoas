d8d0ecefb636c46f6ddd06e024d577c228361a89
Kayro <kayrojesiel99@gmail.com>
2025-10-27 15:54:14 -0300

    Criado a entidade para o banco de dados

--- src/main/java/com/kjdev/CadrastroDePessoas/PessoaController.java
+++ src/main/java/com/kjdev/CadrastroDePessoas/PessoaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class Controller {
+public class PessoaController {
 
     @GetMapping
     public String boasVindas(){
---
+++ src/main/java/com/kjdev/CadrastroDePessoas/PessoaModel.java
@@ -0,0 +1,50 @@
+package com.kjdev.CadrastroDePessoas;
+
+import jakarta.persistence.*;
+
+//Entity transforma uma classe em uma entidade do BD
+@Entity
+@Table(name = "tb_cadastro")
+public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int idade;
+    private int cpf;
+
+    public PessoaModel(){
+
+    }
+
+    public PessoaModel(String nome, int idade, int cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
