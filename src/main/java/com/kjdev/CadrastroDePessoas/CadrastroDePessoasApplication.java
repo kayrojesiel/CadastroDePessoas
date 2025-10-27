d1f2ec77d414eb969e3bade15488334a0efb85ef
Kayro <kayrojesiel99@gmail.com>
2025-10-27 14:22:41 -0300

    first commit

---
+++ src/main/java/com/kjdev/CadrastroDePessoas/CadrastroDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.kjdev.CadrastroDePessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastroDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadrastroDePessoasApplication.class, args);
+	}
+
+}
